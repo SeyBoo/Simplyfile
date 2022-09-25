433b94015f0bd1fceee336e467187925b801094c
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-09-25 17:41:23 +0200

    Configure store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NativeBaseProvider} from 'native-base';
+import store from './common/store';
+import {Provider} from 'react-redux';
 import Navigation from './common/navigation';
 
 const App = () => {
   return (
-    <NativeBaseProvider>
-      <StatusBar barStyle="dark-content" />
-      <Navigation />
-    </NativeBaseProvider>
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <StatusBar barStyle="dark-content" />
+        <Navigation />
+      </NativeBaseProvider>
+    </Provider>
   );
 };
 
