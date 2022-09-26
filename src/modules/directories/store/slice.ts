a59f3c857a1d1e112afb0c7d3239ba6525b18e59
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-09-26 10:10:28 +0200

    Configure store for Directories

---
+++ src/modules/directories/store/slice.ts
@@ -0,0 +1,11 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import initialState from "./state";
+
+const DirectoriesSlice = createSlice({
+  name: "directories",
+  initialState,
+  reducers: {}
+  }
+});
+
+export default DirectoriesSlice;
