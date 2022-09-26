c0a2bc9eec00bae39d0b345429858f6594c60f57
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-09-26 10:12:02 +0200

    Create directories list comp

---
+++ src/modules/directories/components/directoriesList.tsx
@@ -0,0 +1,15 @@
+import {HStack, ScrollView} from "native-base";
+import React  from "react";
+
+export default function DirectoriesList() {
+  return (
+      <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          mt={5}
+      >
+        <HStack>
+        </HStack>
+      </ScrollView>
+  )
+}
