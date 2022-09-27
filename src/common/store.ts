b8f5788e8305004999f0abb20b8470952a98a24e
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-09-27 20:59:48 +0200

    Fix error in dummy data

--- src/common/store.ts
+++ src/common/store.ts
@@ -1,13 +1,11 @@
 import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
 import userSlice from '../modules/auth/store/slice';
 import DirectoriesSlice from '../modules/directories/store/slice';
-import DocumentSlice from "../modules/document/store/slice";
 
 const store = configureStore({
   reducer: {
     [userSlice.name]: userSlice.reducer,
     [DirectoriesSlice.name]: DirectoriesSlice.reducer,
-    [DocumentSlice.name]: DocumentSlice.reducer,
   },
   middleware: getDefaultMiddleware =>
       getDefaultMiddleware({
--- src/modules/document/api/error.ts
+++
@@ -1,3 +0,0 @@
-export class AuthApiError extends Error {}
-
-export class InternalError extends AuthApiError {}
