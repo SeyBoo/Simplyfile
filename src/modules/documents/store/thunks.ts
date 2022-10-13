3a4e583e20c121d501ed2cdf4ea87d63fca6f029
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-13 10:06:32 +0200

    Format code

--- src/modules/documents/store/thunks.ts
+++ src/modules/documents/store/thunks.ts
@@ -1,45 +1,45 @@
-import {AppThunk} from '../../../common/store';
-import {setLastUpdated, setDocument} from '../../documents/store/slice';
-import {getDocumentsBackend} from '../api';
+import { AppThunk } from "../../../common/store";
+import { setLastUpdated, setDocument } from "../../documents/store/slice";
+import { getDocumentsBackend } from "../api";
 
 export const fetchDocument =
-  (uuid: string): AppThunk =>
-  async dispatch => {
-    const backendInstance = await getDocumentsBackend();
-    const document = await backendInstance.fetchDocument(uuid);
-    await dispatch(setDocument({document}));
-  };
+	(uuid: string): AppThunk =>
+	async (dispatch) => {
+		const backendInstance = await getDocumentsBackend();
+		const document = await backendInstance.fetchDocument(uuid);
+		await dispatch(setDocument({ document }));
+	};
 
 export const deleteDocument =
-  (uuid: string): AppThunk =>
-  async () => {
-    const backendInstance = await getDocumentsBackend();
-    await backendInstance.deleteDocument(uuid);
-  };
+	(uuid: string): AppThunk =>
+	async () => {
+		const backendInstance = await getDocumentsBackend();
+		await backendInstance.deleteDocument(uuid);
+	};
 
 export const updateDocumentName =
-  (uuid: string, name: string): AppThunk =>
-  async () => {
-    const backendInstance = await getDocumentsBackend();
-    await backendInstance.updateDocumentName(uuid, name);
-  };
+	(uuid: string, name: string): AppThunk =>
+	async () => {
+		const backendInstance = await getDocumentsBackend();
+		await backendInstance.updateDocumentName(uuid, name);
+	};
 
 export const bookmarkDocument =
-  (uuid: string): AppThunk =>
-  async () => {
-    const backendInstance = await getDocumentsBackend();
-    await backendInstance.bookmarkDocument(uuid);
-  };
+	(uuid: string): AppThunk =>
+	async () => {
+		const backendInstance = await getDocumentsBackend();
+		await backendInstance.bookmarkDocument(uuid);
+	};
 
-export const fetchDocuments = (): AppThunk => async dispatch => {
-  const backendInstance = await getDocumentsBackend();
-  const lastUpdated = await backendInstance.fetchDocuments();
-  await dispatch(setLastUpdated({lastUpdated}));
+export const fetchDocuments = (): AppThunk => async (dispatch) => {
+	const backendInstance = await getDocumentsBackend();
+	const lastUpdated = await backendInstance.fetchDocuments();
+	await dispatch(setLastUpdated({ lastUpdated }));
 };
 
 export const addNewDocument =
-  (name: string, uri: string, dossierUuid: string): AppThunk =>
-  async () => {
-    const backendInstance = await getDocumentsBackend();
-    await backendInstance.addDocument(name, uri, dossierUuid);
-  };
+	(name: string, uri: string, dossierUuid: string): AppThunk =>
+	async () => {
+		const backendInstance = await getDocumentsBackend();
+		await backendInstance.addDocument(name, uri, dossierUuid);
+	};
