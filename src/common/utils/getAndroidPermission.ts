4ccaa1bc499dde2881cc629b7034324e1574c0a9
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-16 20:23:14 +0200

    Add missing files

---
+++ src/common/utils/getAndroidPermission.ts
@@ -0,0 +1,11 @@
+export default async function hasAndroidWritePermission() {
+	const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+	const hasPermission = await PermissionsAndroid.check(permission);
+	if (hasPermission) {
+		return true;
+	}
+
+	const status = await PermissionsAndroid.request(permission);
+	return status === 'granted';
+}
