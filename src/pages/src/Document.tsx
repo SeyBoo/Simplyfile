506477a02602578d7be3aa403ae326143e39e673
Antoine CHEVALIER <53263615+SeyBoo@users.noreply.github.com>
2022-10-19 17:02:25 +0200

    Lint files

--- src/pages/src/Document.tsx
+++ src/pages/src/Document.tsx
@@ -1,26 +1,26 @@
 import React, {
-	FunctionComponent,
-	useCallback,
-	useEffect,
-	useState,
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
 } from 'react';
 import {
-	Box,
-	Button,
-	Center,
-	HStack,
-	Image,
-	Input,
-	Spinner,
-	Text,
+  Box,
+  Button,
+  Center,
+  HStack,
+  Image,
+  Input,
+  Spinner,
+  Text,
 } from 'native-base';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AuthStackParamList } from '../../common/navigation/authRoutes';
 import { useAppDispatch, useAppSelector } from '../../common/hooks/store';
 import {
-	deleteDocument,
-	fetchDocument,
-	updateDocumentName,
+  deleteDocument,
+  fetchDocument,
+  updateDocumentName,
 } from '../../modules/documents/store/thunks';
 import ArrowBack from '../../common/assets/icon/arrow-square-left.png';
 import ExportIcon from '../../common/assets/icon/export.png';
@@ -33,26 +33,27 @@ import BaseLayout from '../../common/layouts/baseLayout';
 import hasAndroidWritePermission from '../../common/utils/getAndroidPermission.ts';
 
 export const Document: FunctionComponent<
-	NativeStackScreenProps<AuthStackParamList, 'Document'>
+NativeStackScreenProps<AuthStackParamList, 'Document'>
 > = ({ route }) => {
-	const { uuid } = route.params;
-	const dispatch = useAppDispatch();
-	const document = useAppSelector((state) => state.documents.document);
-	const navigation = useNavigation();
-	const [name, setName] = useState<string>('');
+  const setAlert = useSetAlert();
+  const { uuid } = route.params;
+  const dispatch = useAppDispatch();
+  const document = useAppSelector((state) => state.documents.document);
+  const navigation = useNavigation();
+  const [name, setName] = useState<string>('');
 
-	const handleFetchDocument = useCallback(async () => {
-		try {
-			await dispatch(fetchDocument(uuid));
-		} catch (e) {
-			console.log(e);
-		}
-	}, [dispatch, uuid]);
+  const handleFetchDocument = useCallback(async () => {
+    try {
+      await dispatch(fetchDocument(uuid));
+    } catch (e) {
+      console.log(e);
+    }
+  }, [dispatch, uuid]);
 
-	async function handleSaveImage() {
-		if (Platform.OS === 'android' && !(await hasAndroidWritePermission())) {
-			return;
-		}
+  async function handleSaveImage() {
+    if (Platform.OS === 'android' && !(await hasAndroidWritePermission())) {
+      return;
+    }
 
 		if (document) {
 			await CameraRoll.save(document?.image).then(() => {
@@ -61,105 +62,105 @@ export const Document: FunctionComponent<
 		}
 	}
 
-	const handleDeleteDocument = async () => {
-		try {
-			await dispatch(deleteDocument(uuid));
-			navigation.goBack();
-		} catch (e) {
-			console.log(e);
-		}
-	};
+  const handleDeleteDocument = async () => {
+    try {
+      await dispatch(deleteDocument(uuid));
+      navigation.goBack();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	const handleNameChange = async () => {
-		try {
-			await dispatch(updateDocumentName(uuid, name));
-			await handleFetchDocument();
-		} catch (e) {
-			console.log(e);
-		}
-	};
+  const handleNameChange = async () => {
+    try {
+      await dispatch(updateDocumentName(uuid, name));
+      await handleFetchDocument();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	useEffect(() => {
-		(async () => handleFetchDocument())();
-	}, [handleFetchDocument]);
+  useEffect(() => {
+    (async () => handleFetchDocument())();
+  }, [handleFetchDocument]);
 
-	if (document === null || document?.uuid !== uuid) {
-		return <Spinner />;
-	}
-	return (
-		<BaseLayout>
-			<HStack justifyContent="space-between" px="3%" mb="2.5%">
-				<Button
-					background="transparent"
-					_pressed={{
-						opacity: 0.5,
-					}}
-					onPress={() => navigation.goBack()}
-				>
-					<Image source={ArrowBack} alt={document.name} />
-				</Button>
-				<HStack>
-					{document.name !== name && name !== '' && (
-						<Button
-							background="transparent"
-							_pressed={{
-								opacity: 0.5,
-							}}
-							onPress={() => handleNameChange()}
-						>
-							<Image source={AcceptChangeIcon} alt="Accept icon" size={7} />
-						</Button>
-					)}
-					<Button
-						background="transparent"
-						_pressed={{
-							opacity: 0.5,
-						}}
-						onPress={() => handleSaveImage()}
-					>
-						<Image source={ExportIcon} alt="Export icon" />
-					</Button>
-					<Button
-						background="transparent"
-						_pressed={{
-							opacity: 0.5,
-						}}
-						onPress={() => handleDeleteDocument()}
-					>
-						<Image source={TrashIcon} alt="trash icon" size={7} />
-					</Button>
-				</HStack>
-			</HStack>
-			<Center>
-				<Image
-					source={{ uri: document.image }}
-					style={{
-						width: '90%',
-						height: 400,
-						borderRadius: 15,
-					}}
-					alt={document.name}
-				/>
-				<Box mt="10" width="90%">
-					<Text fontSize="xl" mb="2">
-						Name
-					</Text>
-					<Input
-						mx="3"
-						autoCorrect={false}
-						autoCapitalize="none"
-						fontSize="md"
-						fontWeight="light"
-						borderRadius="lg"
-						background="white"
-						ml="0"
-						p={6}
-						borderColor="gray.300"
-						defaultValue={document.name}
-						onChangeText={(text) => setName(text)}
-					/>
-				</Box>
-			</Center>
-		</BaseLayout>
-	);
+  if (document === null || document?.uuid !== uuid) {
+    return <Spinner />;
+  }
+  return (
+    <BaseLayout>
+      <HStack justifyContent="space-between" px="3%" mb="2.5%">
+        <Button
+          background="transparent"
+          _pressed={{
+            opacity: 0.5,
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image source={ArrowBack} alt={document.name} />
+        </Button>
+        <HStack>
+          {document.name !== name && name !== '' && (
+            <Button
+              background="transparent"
+              _pressed={{
+                opacity: 0.5,
+              }}
+              onPress={() => handleNameChange()}
+            >
+              <Image source={AcceptChangeIcon} alt="Accept icon" size={7} />
+            </Button>
+          )}
+          <Button
+            background="transparent"
+            _pressed={{
+              opacity: 0.5,
+            }}
+            onPress={() => handleSaveImage()}
+          >
+            <Image source={ExportIcon} alt="Export icon" />
+          </Button>
+          <Button
+            background="transparent"
+            _pressed={{
+              opacity: 0.5,
+            }}
+            onPress={() => handleDeleteDocument()}
+          >
+            <Image source={TrashIcon} alt="trash icon" size={7} />
+          </Button>
+        </HStack>
+      </HStack>
+      <Center>
+        <Image
+          source={{ uri: document.image }}
+          style={{
+            width: '90%',
+            height: 400,
+            borderRadius: 15,
+          }}
+          alt={document.name}
+        />
+        <Box mt="10" width="90%">
+          <Text fontSize="xl" mb="2">
+            Name
+          </Text>
+          <Input
+            mx="3"
+            autoCorrect={false}
+            autoCapitalize="none"
+            fontSize="md"
+            fontWeight="light"
+            borderRadius="lg"
+            background="white"
+            ml="0"
+            p={6}
+            borderColor="gray.300"
+            defaultValue={document.name}
+            onChangeText={(text) => setName(text)}
+          />
+        </Box>
+      </Center>
+    </BaseLayout>
+  );
 };
--- src/pages/src/Home.tsx
+++ src/pages/src/Home.tsx
@@ -9,97 +9,97 @@ import CreateNewDirectory from '../../modules/directories/components/createNewDi
 import DocumentsList from '../../modules/documents/components/documentsList';
 
 export const Home: FunctionComponent = () => {
-	const dispatch = useAppDispatch();
-	const directories = useAppSelector((state) => state.directories.directories);
-	const lastUpdatedDocuments = useAppSelector(
-		(state) => state.documents.lastUpdated
-	);
+  const dispatch = useAppDispatch();
+  const directories = useAppSelector((state) => state.directories.directories);
+  const lastUpdatedDocuments = useAppSelector(
+    (state) => state.documents.lastUpdated
+  );
 
-	const handleFetchLastUpdatedDocuments = useCallback(async () => {
-		try {
-			await dispatch(fetchLastUpdatedDocuments());
-		} catch (e) {
-			console.log(e);
-		}
-	}, [dispatch]);
+  const handleFetchLastUpdatedDocuments = useCallback(async () => {
+    try {
+      await dispatch(fetchLastUpdatedDocuments());
+    } catch (e) {
+      console.log(e);
+    }
+  }, [dispatch]);
 
-	const handleFetchDirectories = useCallback(async () => {
-		try {
-			await dispatch(fetchDirectories());
-		} catch (e) {
-			console.log(e);
-		}
-	}, [dispatch]);
+  const handleFetchDirectories = useCallback(async () => {
+    try {
+      await dispatch(fetchDirectories());
+    } catch (e) {
+      console.log(e);
+    }
+  }, [dispatch]);
 
-	const handleFetchHomeData = async () => {
-		await handleFetchLastUpdatedDocuments();
-		await handleFetchDirectories();
-	};
+  const handleFetchHomeData = async () => {
+    await handleFetchLastUpdatedDocuments();
+    await handleFetchDirectories();
+  };
 
-	useEffect(() => {
-		(async () => handleFetchHomeData())();
-	}, [handleFetchHomeData]);
+  useEffect(() => {
+    (async () => handleFetchHomeData())();
+  }, [handleFetchHomeData]);
 
-	return (
-		<PageLayout
-			title="Search"
-			subtitle="for your files"
-			refetch={handleFetchHomeData}
-		>
-			{/*<HStack*/}
-			{/*    mt="7.5%"*/}
-			{/*>*/}
-			{/*  <Input*/}
-			{/*      placeholder="Find a file"*/}
-			{/*      borderRadius="xl"*/}
-			{/*      backgroundColor="white"*/}
-			{/*      width="60%"*/}
-			{/*      py={3}*/}
-			{/*      fontSize="xl"*/}
-			{/*      mr="5%"*/}
-			{/*      color="light.400"*/}
-			{/*      InputLeftElement={<Icon as={<MaterialIcons name="search"/>} color="light.400" size={8} ml={3}/>}*/}
-			{/*  />*/}
-			{/*  <Button*/}
-			{/*      backgroundColor="#E4E4E4"*/}
-			{/*      alignItems="center"*/}
-			{/*      borderRadius="2xl"*/}
-			{/*      px={4}*/}
-			{/*      _pressed={{*/}
-			{/*        opacity: '.5'*/}
-			{/*      }}*/}
-			{/*  >*/}
-			{/*    <Image*/}
-			{/*        source={Filter}*/}
-			{/*        width="5"*/}
-			{/*        height="7"*/}
-			{/*        alt="filter"*/}
-			{/*    />*/}
-			{/*  </Button>*/}
-			{/*</HStack>*/}
-			<Box>
-				<Text fontSize="3xl" fontWeight="bold" mt={10}>
-					Directory
-				</Text>
-				<ScrollView horizontal showsHorizontalScrollIndicator={false} mt={5}>
-					<HStack>
-						<CreateNewDirectory />
-						<DirectoriesList directories={directories} />
-					</HStack>
-				</ScrollView>
-			</Box>
-			<Box mt={10} mb="30%">
-				<Text fontSize="3xl" fontWeight="bold">
-					Last updated
-				</Text>
-				<ScrollView horizontal showsHorizontalScrollIndicator={false}>
-					<DocumentsList
-						documents={lastUpdatedDocuments}
-						handleFetchDirectory={handleFetchLastUpdatedDocuments}
-						spacing="10"
-					/>
-				</ScrollView>
-			</Box>
-		</PageLayout>
-	);
+  return (
+    <PageLayout
+      title="Search"
+      subtitle="for your files"
+      refetch={handleFetchHomeData}
+    >
+      {/*<HStack*/}
+      {/*    mt="7.5%"*/}
+      {/*>*/}
+      {/*  <Input*/}
+      {/*      placeholder="Find a file"*/}
+      {/*      borderRadius="xl"*/}
+      {/*      backgroundColor="white"*/}
+      {/*      width="60%"*/}
+      {/*      py={3}*/}
+      {/*      fontSize="xl"*/}
+      {/*      mr="5%"*/}
+      {/*      color="light.400"*/}
+      {/*      InputLeftElement={<Icon as={<MaterialIcons name="search"/>} color="light.400" size={8} ml={3}/>}*/}
+      {/*  />*/}
+      {/*  <Button*/}
+      {/*      backgroundColor="#E4E4E4"*/}
+      {/*      alignItems="center"*/}
+      {/*      borderRadius="2xl"*/}
+      {/*      px={4}*/}
+      {/*      _pressed={{*/}
+      {/*        opacity: '.5'*/}
+      {/*      }}*/}
+      {/*  >*/}
+      {/*    <Image*/}
+      {/*        source={Filter}*/}
+      {/*        width="5"*/}
+      {/*        height="7"*/}
+      {/*        alt="filter"*/}
+      {/*    />*/}
+      {/*  </Button>*/}
+      {/*</HStack>*/}
+      <Box>
+        <Text fontSize="3xl" fontWeight="bold" mt={10}>
+          Directory
+        </Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} mt={5}>
+          <HStack>
+            <CreateNewDirectory />
+            <DirectoriesList directories={directories} />
+          </HStack>
+        </ScrollView>
+      </Box>
+      <Box mt={10} mb="30%">
+        <Text fontSize="3xl" fontWeight="bold">
+          Last updated
+        </Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <DocumentsList
+            documents={lastUpdatedDocuments}
+            handleFetchDirectory={handleFetchLastUpdatedDocuments}
+            spacing="10"
+          />
+        </ScrollView>
+      </Box>
+    </PageLayout>
+  );
 };
